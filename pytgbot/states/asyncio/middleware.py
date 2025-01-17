636a384cf17ff739d4421e452c460e6ae99f92d4
The Hamker Guy <thehamkerguy@gmail.com>
2025-01-17 20:29:52 +0530

    Update middleware.py

--- pytgbot/states/asyncio/middleware.py
+++ pytgbot/states/asyncio/middleware.py
@@ -1,16 +1,16 @@
-from telebot.asyncio_handler_backends import BaseMiddleware
-from telebot.async_telebot import AsyncTeleBot
-from telebot.states.sync.context import StateContext
-from telebot.util import update_types
-from telebot import types
+from pytgbot.asyncio_handler_backends import BaseMiddleware
+from pytgbot.async_telebot import AsyncPytgbot
+from pytgbot.states.sync.context import StateContext
+from pytgbot.util import update_types
+from pytgbot import types
 
 
 class StateMiddleware(BaseMiddleware):
 
-    def __init__(self, bot: AsyncTeleBot) -> None:
+    def __init__(self, bot: AsyncPytgbot) -> None:
         self.update_sensitive = False
         self.update_types = update_types
-        self.bot: AsyncTeleBot = bot
+        self.bot: AsyncPytgbot = bot
 
     async def pre_process(self, message, data):
         state_context = StateContext(message, self.bot)
